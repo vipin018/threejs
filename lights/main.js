4605ddeeb7bccce230996d2698d0d076733ba0d5
vipin018 <vipinpathak018@gmail.com>
2024-12-31 20:40:57 +0530

    lights

--- lights/main.js
+++ lights/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
-
+import GUI from 'lil-gui'; // Import the dat.gui library
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -15,13 +15,29 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio), 2);
 
+// lights
+let AmbientLightlight = new THREE.AmbientLight( "white", 1); // it is used when you need equal amount of light everywhere. (color and intensity)
+scene.add(AmbientLightlight);
 
-const geometry = new THREE.BoxGeometry(1, 2, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+const DirectionalLight = new THREE.DirectionalLight("white", 5); // it is used when you need light to be fall from a particular direction. (color and intensity)
+DirectionalLight.position.set(2, -1, -1); // position of the light in space.
+scene.add(DirectionalLight);
+
+const helper = new THREE.DirectionalLightHelper(DirectionalLight,0.2);
+scene.add(helper);
+
+const geometry = new THREE.BoxGeometry(2, 2, 1);
+const material = new THREE.MeshPhysicalMaterial({ color: "red" });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+const gui = new GUI();
+const lightFolder = gui.addFolder('Directional Light');
+lightFolder.add(DirectionalLight.position, 'x', -10, 10).name('Light X Position');
+lightFolder.add(DirectionalLight.position, 'y', -10, 10).name('Light Y Position');
+lightFolder.open(); // Open the folder by default
 
 function animate() {
     requestAnimationFrame(animate);
