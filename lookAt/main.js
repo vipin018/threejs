c5c0b0bdd5c5d71738400786db9640eb2dbd27ed
vipin018 <vipinpathak018@gmail.com>
2024-12-31 19:52:43 +0530

    orbit controls

--- lookAt/main.js
+++ lookAt/main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { color } from 'three/tsl';
-// Create scene, camera and renderer
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -13,7 +12,6 @@ camera.position.z = 4;
 const canvas = document.querySelector('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio), 2);
 
 
@@ -22,25 +20,25 @@ const material = new THREE.MeshBasicMaterial({ color: "red" });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-// cube.lookAt(-1,-1,0);
-// cube.lookAt(new THREE.Vector3(-0.1, 0.1, 0.1));
 
-const mouse = {
-    x: 0,
-    y: 0,
-}
+// const mouse = {
+//     x: 0,
+//     y: 0,
+// }
 
-window.addEventListener('mousemove', function (e) {
-    mouse.x = (e.clientX / window.innerWidth) * 2 - 1; // this referes to the mouse position when mouse is moved from left to right
-    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1; // this referes to the mouse position when mouse is moved from top to bottom
-    cube.rotation.y = mouse.x;
-    cube.rotation.x = -mouse.y;
-})
+// window.addEventListener('mousemove', function (e) {
+//     mouse.x = (e.clientX / window.innerWidth) * 2 - 1; // this referes to the mouse position when mouse is moved from left to right
+//     mouse.y = -(e.clientY / window.innerHeight) * 2 + 1; // this referes to the mouse position when mouse is moved from top to bottom
+//     cube.rotation.y = mouse.x;
+//     cube.rotation.x = -mouse.y;
+// })
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 function animate() {
     requestAnimationFrame(animate);
-    // controls.update();
+    controls.update();
     renderer.render(scene, camera);
 }
 
