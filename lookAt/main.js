8ae61d62907d422227ff5021a7cec4372e5fffe7
vipin018 <vipinpathak018@gmail.com>
2024-12-31 18:56:34 +0530

    updated

--- lookAt/main.js
+++ lookAt/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { color } from 'three/tsl';
 // Create scene, camera and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,19 +15,20 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 // document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial();
+const geometry = new THREE.BoxGeometry(1, 2, 1);
+const material = new THREE.MeshBasicMaterial({color:"red"});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+// cube.lookAt(-1,-1,0);
+// cube.lookAt(new THREE.Vector3(-0.1, 0.1, 0.1));
 
-// Position camera
-renderer.render(scene, camera);
-
-// Animation loop
-let clock = new THREE.Clock();
+const mouse = {
+    x: 0,
+    y: 0,
+}
 
 function animate() {
     requestAnimationFrame(animate);
